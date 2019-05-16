7609c85930447c69518a40a2efe04aabd5cec4f4
hongchiong <hongchiong@gmail.com>
2019-05-16 14:14:03 +0800

    limit 255 characters. voting buttons. save votes in state.

--- src/components/TopicCard.js
+++ src/components/TopicCard.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import { Card, CardText, CardBody,
+  CardTitle, Button, ButtonGroup } from 'reactstrap'; 
 
 class TopicCard extends Component {
   render() {
+    const { content, votes } = this.props.topic;
+    console.log(this.props, content, votes)
     return (
-      <div>
-        {this.props.topic}
-      </div>
+      <Card>
+        <CardBody className="card-layout">
+          <CardTitle>Votes: {votes}</CardTitle>
+          <CardText>{content}</CardText>
+          <ButtonGroup>
+            <Button color="success">👍</Button>
+            <Button color="danger">👎</Button>
+          </ButtonGroup>
+        </CardBody>
+      </Card>
     )
   }
 }
--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
-import TopicCard from './TopicCard'
-import { ListGroup, ListGroupItem, Form, FormGroup, Label, Input, Button } from 'reactstrap'
+// import TopicCard from './TopicCard'
+import { ListGroup, ListGroupItem, Form, FormGroup, Label, Input, Button, Card, CardText, CardBody,
+  CardTitle, ButtonGroup } from 'reactstrap'
 
+const TopicCard = ({index, topic, upVote, downVote}) => (
+  <Card>
+    <CardBody className="card-layout">
+      <CardTitle>Votes: {topic.votes}</CardTitle>
+      <CardText>{topic.content}</CardText>
+      <ButtonGroup>
+        <Button color="success" onClick={upVote} index={index}>👍</Button>
+        <Button color="danger" onClick={downVote} index={index}>👎</Button>
+      </ButtonGroup>
+    </CardBody>
+  </Card>
+);
 class Topics extends Component {
   constructor(props) {
 		super(props);
@@ -15,7 +28,17 @@ class Topics extends Component {
     this.setState({content: e.target.value});
   }
 
-  addTopic = (e) => {
+  descendingSort = (a, b) => {
+    if (a.votes > b.votes) {
+      return -1;
+    }
+    if (b.votes > a.votes) {
+      return 1;
+    }
+    return 0;
+  }
+
+  addTopic = () => {
     this.setState(state => {
       const newTopic = {
         content: this.state.content, 
@@ -23,22 +46,36 @@ class Topics extends Component {
       };
       
       state.topics.push(newTopic);
-      const topics = state.topics;
+      const topics = state.topics.sort(this.descendingSort);;
 
       return {
         topics,
         content: ""
       };
     });
+  }
 
-    console.log(this.state.topics);
+  upVote = (e) => {
+    const topic = e.target.getAttribute("index");
+    let topics = this.state.topics;
+    topics[topic].votes += 1;
+    topics.sort(this.descendingSort);
+    this.setState({topics: topics});
+  }
+
+  downVote = (e) => {
+    const topic = e.target.getAttribute("index");
+    let topics = this.state.topics;
+    topics[topic].votes -= 1;
+    topics.sort(this.descendingSort);
+    this.setState({topics: topics});
   }
   
   render() {
     const topics = this.state.topics.map((topic, key) => {
       return (
         <ListGroupItem key={key}>
-          <TopicCard topic={topic.content} />
+          <TopicCard index={key} topic={topic} upVote={this.upVote} downVote={this.downVote}/>
         </ListGroupItem>
         );
     })
@@ -49,7 +86,7 @@ class Topics extends Component {
           <Form>
             <FormGroup>
               <Label for="topicConent">Add New Topic</Label>
-              <Input type="textarea" name="text" id="topicConent" onChange={this.handleChange} value={this.state.content}/>
+              <Input type="textarea" name="text" id="topicConent" maxlength="255" onChange={this.handleChange} value={this.state.content}/>
             </FormGroup>
             <Button color="primary" onClick={this.addTopic}>Submit Topic</Button>
           </Form>
